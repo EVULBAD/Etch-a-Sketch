d6e8fca9e9da52efcd3050e2b1aa7c254fdcad20
Dmitry <dmitrydohy@gmail.com>
2023-02-10 15:53:10 -0800

    Grid children change color
    On mouse hover, grid children will change from white to black. The clear button clears the grid, resetting each child to white. The grid size button is a work-in-progress and currently does not work.

--- main.js
+++ main.js
@@ -1,18 +1,57 @@
 //declarations
-const square = document.createElement("square")
+const square = document.createElement("div"),
     grid = document.querySelector("#grid");
+let gridSize = 16;
 
-//adding classes and style
-square.classList.add("square");
-grid.setAttribute("style", "display: grid; grid-template-columns: repeat(16, 1fr); grid-template-rows: repeat(16, 1fr);")
+function gridSetup() {
+    //adjusting grid size
+    square.classList.add("square");
+    grid.setAttribute("style", "display: grid; grid-template-columns: repeat(" + gridSize + ", 1fr); grid-template-rows: repeat(" + gridSize + ", 1fr);")
 
-//appending the divs
-for (i = 0; i < (16 * 16); i++) {
-    grid.appendChild(square.cloneNode(true));
+    //appending the divs containing each square
+    for (i = 0; i < (gridSize * gridSize); i++) {
+        grid.appendChild(square.cloneNode(true));
+    }
+
+    //adjusting borders
+    grid.children[0].classList.add("no-left-border");
+    for (i = 0; i < (gridSize * gridSize); i++) {
+        if (i < gridSize) {
+            grid.children[i].classList.add("no-top-border");
+        } else if (i == gridSize || i % gridSize == 0) {
+            grid.children[i].classList.add("no-left-border");
+        }
+    }
 }
-for (i = 0; i < 16; i++) {
-    grid.children[i].classList.add("first-row");
+
+//open default 16x16 grid on page load
+window.onload = gridSetup();
+
+//adding color to squares
+let squareClick = document.querySelectorAll(".square");
+
+for (i = 0; i < squareClick.length; i++) {
+    let sqr = squareClick[i];
+    sqr.addEventListener("mouseover", function color() {
+        sqr.setAttribute("style", "background: black");
+    });
 }
-for (i = 0; i < (16 * 16); i += 16) {
-    grid.children[i].classList.add("first-col");
-}
+
+//buttons
+//  "size" button; resizes the grid according to user prompt
+/*document.querySelector(".size").addEventListener("click", function size() {
+    gridSize = prompt("Grid size:");
+    if (gridSize > 64) {
+        gridSize = 64;
+    }
+    grid.innerHTML = "";
+    gridSetup();
+});*/
+
+//  "clear" button; clears the grid
+document.querySelector(".clear").addEventListener("click",function clear(){
+    for (i = 0; i < (gridSize * gridSize); i++) {
+        let sqr = squareClick[i];
+        sqr.removeAttribute("style");
+    }
+});
