616b8eb1fdee6d9bd5537559fb646c5bc7928106
Dmitry <dmitrydohy@gmail.com>
2023-02-11 11:05:13 -0800

    Button makes new grid according to input
    The button labelled "grid size" updates the number of squares on the grid according to user input from prompt. Buttons got new styling. Also, created a rainbow button; button doesn't do anything yet, but received special styling.

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 //declarations
 const square = document.createElement("div"),
     grid = document.querySelector("#grid");
-let gridSize = 16;
+let gridSize = 16,
+    rainbow;
 
 function gridSetup() {
     //adjusting grid size
@@ -22,34 +23,45 @@ function gridSetup() {
             grid.children[i].classList.add("no-left-border");
         }
     }
+
+    //adding color to squares
+    let squareClick = document.querySelectorAll(".square");
+
+    for (i = 0; i < squareClick.length; i++) {
+        let sqr = squareClick[i];
+        sqr.addEventListener("mouseover", function blackFill() {
+            sqr.setAttribute("style", "background: black");
+        });
+    }
 }
+
 
 //open default 16x16 grid on page load
 window.onload = gridSetup();
 
-//adding color to squares
-let squareClick = document.querySelectorAll(".square");
-
-for (i = 0; i < squareClick.length; i++) {
-    let sqr = squareClick[i];
-    sqr.addEventListener("mouseover", function color() {
-        sqr.setAttribute("style", "background: black");
-    });
-}
-
 //buttons
 //  "size" button; resizes the grid according to user prompt
-/*document.querySelector(".size").addEventListener("click", function size() {
+document.querySelector(".size").addEventListener("click", function size() {
     gridSize = prompt("Grid size:");
     if (gridSize > 64) {
         gridSize = 64;
+    } else if (gridSize == 0 || gridSize == null) {
+        gridSize = 16;
     }
     grid.innerHTML = "";
     gridSetup();
-});*/
+});
+
+//  "rainbow" button; makes squares get colored with random color instead of black
+/*document.querySelector(".rainbow").addEventListener("click",function clear(){
+    rainbow = true;
+});
+
+console.log(rainbow);*/
 
 //  "clear" button; clears the grid
 document.querySelector(".clear").addEventListener("click",function clear(){
+    let squareClick = document.querySelectorAll(".square");
     for (i = 0; i < (gridSize * gridSize); i++) {
         let sqr = squareClick[i];
         sqr.removeAttribute("style");
